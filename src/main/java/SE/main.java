baa8da45ee9274e4bdccee47b8657fd4fc96698c
朱峻平 <a5824384@gmail.com>
2019-12-12 18:05:08 +0800

    Check IN

--- src/main/java/SE/main.java
+++ src/main/java/SE/main.java
@@ -3,5 +3,17 @@ package SE;
 public class main {
     public static void main(String args[]) {
         SE_Class SE = new SE_Class();
+        char score = SE.letterGrade(94);
+        score = SE.letterGrade(84);
+        score = SE.letterGrade(74);
+        score = SE.letterGrade(64);
+        score = SE.letterGrade(54);
+        score = SE.letterGrade(44);
+        score = SE.letterGrade(34);
+        score = SE.letterGrade(24);
+        score = SE.letterGrade(14);
+        score = SE.letterGrade(4);
+        score = SE.letterGrade(-4);
+        System.out.println("SE PASS"); // Check IN
     }
 }
